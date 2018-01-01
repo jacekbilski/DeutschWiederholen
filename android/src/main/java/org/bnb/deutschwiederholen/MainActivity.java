cda50834092a754548730493b162bc48f6c9d619
Jacek Bilski <jacek.bilski@gmail.com>
2018-01-01 22:13:10 +0100

    AndroidApp -> added FloatingActionButton, reacts on clicks.

--- android/src/main/java/org/bnb/deutschwiederholen/MainActivity.java
+++ android/src/main/java/org/bnb/deutschwiederholen/MainActivity.java
@@ -2,6 +2,9 @@ package org.bnb.deutschwiederholen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+    fab.setOnClickListener(
+        new View.OnClickListener() {
+
+          @Override
+          public void onClick(View v) {
+            Log.d("tag", "Clicked");
+          }
+        }
+    );
   }
 }
