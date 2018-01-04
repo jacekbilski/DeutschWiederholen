eac08bc983eddd99d12906f93e5eadbced6ba91e
Jacek Bilski <jacek.bilski@innoq.com>
2018-01-04 18:47:40 +0100

    New activity, to which I can navigate.

--- android/src/main/java/org/bnb/deutschwiederholen/MainActivity.java
+++ android/src/main/java/org/bnb/deutschwiederholen/MainActivity.java
@@ -1,9 +1,11 @@
 package org.bnb.deutschwiederholen;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +14,12 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     FloatingActionButton fab = findViewById(R.id.floatingActionButton);
-    fab.setOnClickListener(v -> Log.d("tag", "Clicked"));
+    fab.setOnClickListener(this::fabOnClick);
+  }
+
+  private void fabOnClick(View view) {
+    Log.d("tag", "Clicked");
+    Intent intent = new Intent(this, QuestionActivity.class);
+    startActivity(intent);
   }
 }
