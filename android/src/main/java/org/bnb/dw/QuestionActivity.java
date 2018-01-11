fbf76608bc83c59377d07a77c750b6ea8218bc03
Jacek Bilski <jacek.bilski@innoq.com>
2018-01-11 11:34:25 +0100

    QuestionActivity has just one instance.

--- android/src/main/java/org/bnb/dw/QuestionActivity.java
+++ android/src/main/java/org/bnb/dw/QuestionActivity.java
@@ -1,5 +1,7 @@
 package org.bnb.dw;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,6 +10,7 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.bnb.dw.wrapper.Choice;
 import org.bnb.dw.wrapper.Question;
@@ -20,16 +23,30 @@ public class QuestionActivity extends AppCompatActivity {
   private RadioGroup radioGroup;
   private Question question;
 
+  private static final String LOG_TAG = "QuestionActivity";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_question);
+    displayQuestion();
+  }
+
+  private void displayQuestion() {
     Button checkButton = findViewById(R.id.checkButton);
     checkButton.setOnClickListener(this::check);
     radioGroup = findViewById(R.id.answersRadioGroup);
     question = fetchQuestion();
     printQuestion();
-    addChoices();
+    setChoices();
+    checkButton.setText(R.string.check);
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    Log.d(LOG_TAG, "onNewIntent");
+    displayQuestion();
   }
 
   private void printQuestion() {
@@ -37,7 +54,8 @@ public class QuestionActivity extends AppCompatActivity {
     text.setText(this.question.text);
   }
 
-  private void addChoices() {
+  private void setChoices() {
+    radioGroup.removeAllViews();
     for (Choice choice : question.choices) {
       RadioButton newRadioButton = new RadioButton(this);
       newRadioButton.setText(choice.text);
@@ -48,19 +66,30 @@ public class QuestionActivity extends AppCompatActivity {
 
   private void check(View view) {
     int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
-    Log.d("questions", "checkedRadioButtonId: " + checkedRadioButtonId);
+    Log.d(LOG_TAG, "checkedRadioButtonId: " + checkedRadioButtonId);
     if (checkedRadioButtonId != -1) {
       RadioButton checkedRadioButton = findViewById(checkedRadioButtonId);
-      Log.d("questions", "checkedRadioButton: " + checkedRadioButton);
-      Log.d("questions", "checkedRadioButton.tag: " + checkedRadioButton.getTag());
+      Log.d(LOG_TAG, "checkedRadioButton: " + checkedRadioButton);
+      Log.d(LOG_TAG, "checkedRadioButton.tag: " + checkedRadioButton.getTag());
       boolean result = verify(question, checkedRadioButton.getTag());
-      Log.d("questions", "Correct? " + result);
+      Log.d(LOG_TAG, "Correct? " + result);
       Button checkButton = findViewById(R.id.checkButton);
+      Context context = getApplicationContext();
+      CharSequence text;
       if (result) {
-        checkButton.setText("OK");
+        text = "OK";
+        checkButton.setText(R.string.next);
+        checkButton.setOnClickListener(this::nextQuestion);
       } else {
-        checkButton.setText("Errr....");
+        text = "Errr....";
       }
+      Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+      toast.show();
     }
+  }
+
+  private void nextQuestion(View view) {
+    Intent intent = new Intent(this, QuestionActivity.class);
+    startActivity(intent);
   }
 }
