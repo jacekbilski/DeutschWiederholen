8b7255e19e517bf3621f02a0c792be0cc068cb07
Jacek Bilski <jacek.bilski@gmail.com>
2018-01-18 09:03:08 +0100

    Getting data from Internet.

--- android/src/main/java/org/bnb/dw/MainActivity.java
+++ android/src/main/java/org/bnb/dw/MainActivity.java
@@ -5,7 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
-import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends Activity {
 
@@ -14,12 +20,25 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     FloatingActionButton fab = findViewById(R.id.floatingActionButton);
-    fab.setOnClickListener(this::fabOnClick);
+    fab.setOnClickListener(v -> fabOnClick());
+    Button updateButton = findViewById(R.id.updateButton);
+    updateButton.setOnClickListener(v -> updateData());
   }
 
-  private void fabOnClick(View view) {
+  private void fabOnClick() {
     Log.d("tag", "Clicked");
     Intent intent = new Intent(this, QuestionActivity.class);
     startActivity(intent);
+  }
+
+  private void updateData() {
+    TextView textView = findViewById(R.id.textView);
+    RequestQueue queue = Volley.newRequestQueue(this);
+    String url = "https://github.com/jacekbilski/DeutschWiederholen/raw/master/build.gradle";
+
+    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        response -> textView.setText("Response is: " + response),
+        error -> textView.setText("That didn't work!"));
+    queue.add(stringRequest);
   }
 }
