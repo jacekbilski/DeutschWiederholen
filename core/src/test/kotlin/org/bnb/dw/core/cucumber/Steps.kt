89e4011737576ee89ba50065d92d5ddf325d8c6d
Jacek Bilski <jacek.bilski@innoq.com>
2018-04-18 18:54:03 +0200

    Test red.

--- core/src/test/kotlin/org/bnb/dw/core/cucumber/Steps.kt
+++ core/src/test/kotlin/org/bnb/dw/core/cucumber/Steps.kt
@@ -2,25 +2,46 @@ package org.bnb.dw.core.cucumber
 
 import cucumber.api.java8.En
 import org.assertj.core.api.Assertions.assertThat
+import org.bnb.dw.core.*
 
 class Steps : En {
 
-    private var noun: String = ""
-    private var gender: String = ""
-    private var choice: String = ""
+    private var result: Boolean = false
+
+    private var quiz: Quiz = Quiz(ArrayList())
+
+    private var nouns: MutableMap<String, Noun> = mutableMapOf()
+    private lateinit var noun: Noun
 
     init {
-        Given("^a noun (.+) with a gender (.+)$") { noun: String, gender: String ->
-            this.noun = noun
-            this.gender = gender
+        Given("^a noun (.+) with a gender (.+)$") { word: String, gender: String ->
+            noun = Noun(word, Gender.valueOf(gender.toUpperCase()))
+            nouns[word] = noun
+        }
+
+        Given("^a noun (.+) with a translation (.+)$") {word: String, translation: String ->
+            noun = Noun(word, Gender.MASCULINE)
+            nouns[word] = noun
         }
 
         When("the user chooses gender (.+)") { choice: String ->
-            this.choice = choice
+            val goodChoice = Choice("", noun.gender)
+            val currentChoice = Choice("", Gender.valueOf(choice.toUpperCase()))
+            val choices = setOf(goodChoice, currentChoice)
+            val question = Question(noun, choices)
+            result = quiz.verify(question, currentChoice)
+        }
+
+        When("the user chooses noun (.+)") { choice: String ->
+            val goodChoice = Choice("", noun)
+            val currentChoice = Choice("", Noun(choice, Gender.MASCULINE))
+            val choices = setOf(goodChoice, currentChoice)
+            val question = Question(noun, choices)
+            result = quiz.verify(question, currentChoice)
         }
 
         Then("the answer was correct: (\\w+)") { correct: Boolean ->
-            assertThat(choice == gender).isEqualTo(correct)
+            assertThat(result).isEqualTo(correct)
         }
     }
 }
--- core/src/test/resources/features/nouns.feature
+++ core/src/test/resources/features/nouns.feature
@@ -10,3 +10,12 @@ Feature: Nouns
       | Gabel     | feminine  | feminine  | true    |
       | Schlüssel | masculine | masculine | true    |
       | Auto      | neuter    | feminine  | false   |
+
+  Scenario Outline: System tests for proper noun when translation presented
+    Given a noun <noun> with a translation <translation>
+    When the user chooses noun <choice>
+    Then the answer was correct: <correct>
+    Examples:
+      | noun     | translation             | choice | correct |
+      | Flut     | powódź                  | Flut   | true    |
+      | Umgebung | środowisko, environment | Gabel  | false   |
