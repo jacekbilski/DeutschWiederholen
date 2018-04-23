8aad28b87838c6b829b94987347ec099227ec0ba
Jacek Bilski <jacek.bilski@gmail.com>
2018-04-23 21:53:22 +0200

    No need for downloading data from Internet and for Volley.

--- android/src/main/kotlin/org/bnb/dw/MainActivity.kt
+++ android/src/main/kotlin/org/bnb/dw/MainActivity.kt
@@ -1,15 +1,10 @@
 package org.bnb.dw
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
-import java.nio.charset.Charset
 
 class MainActivity : Activity() {
 
@@ -17,30 +12,11 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         startQuizFloatingButton.setOnClickListener { startQuizFab() }
-        updateButton.setOnClickListener { updateData() }
     }
 
     private fun startQuizFab() {
         Log.d("tag", "Clicked")
         val intent = Intent(this, QuestionActivity::class.java)
         startActivity(intent)
-    }
-
-    private fun updateData() {
-        val queue = Volley.newRequestQueue(this)
-        val url = "https://github.com/jacekbilski/DeutschWiederholen/raw/master/data/nouns.csv"
-
-        val stringRequest = StringRequest(Request.Method.GET, url,
-                { response -> save("nouns.csv", response) },
-                { error -> Log.e(this.localClassName, "Getting nouns failed", error) })
-        queue.add(stringRequest)
-    }
-
-    private fun save(filename: String, response: String?) {
-        deleteFile(filename)
-        val fos = openFileOutput(filename, Context.MODE_PRIVATE)
-        fos.write((response ?: "").toByteArray(Charset.forName("UTF-8")))
-        fos.close()
-        Log.d(this.localClassName, "Nouns updated")
     }
 }
